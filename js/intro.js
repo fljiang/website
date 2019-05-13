6f794f65bd37e231ef43743cdcf286a507dde44f
Felicia Jiang <fljiang@edu.uwaterloo.ca>
2019-05-13 13:01:46 -0400

    Image and background

--- js/intro.js
+++ js/intro.js
@@ -1,25 +1,25 @@
 // Zoom in and loading animation
-setTimeout(() => {
-    // change opacity + scaling
-    $('.loading__animation').addClass('hidden');
-    $('.loading').addClass('scale');
-    setTimeout(() => {
-        // show all markups inside main page
-        $('.hidden-display').removeClass('hidden-display');
-    }, 200);
-    setTimeout(() => {
-        $('.loading').addClass('hidden');
-        $('.overflow').removeClass('overflow');
-    }, 500);
-    // completely remove display after opacity is 0
-    setTimeout(() => {
-        $('.loading').css(
-            'display', 'none'
-        );
-        $('.hidden').css(
-            'display', 'none'
-        );
-    }, 800);
-    // load on top
-    window.scrollTo(0, 0);
-}, 1000);
+// setTimeout(() => {
+//     // change opacity + scaling
+//     $('.loading__animation').addClass('hidden');
+//     $('.loading').addClass('scale');
+//     setTimeout(() => {
+//         // show all markups inside main page
+//         $('.hidden-display').removeClass('hidden-display');
+//     }, 200);
+//     setTimeout(() => {
+//         $('.loading').addClass('hidden');
+//         $('.overflow').removeClass('overflow');
+//     }, 500);
+//     // completely remove display after opacity is 0
+//     setTimeout(() => {
+//         $('.loading').css(
+//             'display', 'none'
+//         );
+//         $('.hidden').css(
+//             'display', 'none'
+//         );
+//     }, 800);
+//     // load on top
+//     window.scrollTo(0, 0);
+// }, 1000);
